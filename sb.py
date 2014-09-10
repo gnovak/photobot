c5553924fbb3ffaf21c8b4428d9ef84aacf971c2
Greg Novak <greg.novak@gmail.com>
2014-09-10 15:29:11 -0700

    Run through webscope and pull out images w/ certain tags

--- sb.py
+++ sb.py
@@ -21,3 +21,46 @@ def count_tags(input, partial=None):
                 result[tag] = 1
     return result
             
+def fetch_all_beach_ids():
+    fns = ['beaches-%d.pkl'%ii for ii in range(7)]    
+    result = []
+    for fn in fns:
+        dat = util.uncan(fn)
+        partial = fetch_beach_ids(dat)
+        result += partial
+    util.can(result, 'webscope-beach-tagged-set.pkl')
+    ids = [el['photo-id'] for el in result]
+    util.can(ids, 'webscope-beach-tagged-ids.pkl')
+
+def fetch_beach_ids(dat):
+    result = []
+    for el in dat:
+        tags = el['user-tags']
+        if (('beach' in tags and
+             ('ocean' in tags or 'sea' in tags) and 
+             'people' not in tags and
+             'family' not in tags) and 
+            not el['video']):
+            result.append(el)
+    return result
+
+def fetch_all_mountain_ids():
+    fns = ['landscapes-%d.pkl'%ii for ii in range(3)]    
+    result = []
+    for fn in fns:
+        dat = util.uncan(fn)
+        partial = fetch_mountain_ids(dat)
+        result += partial
+    util.can(result, 'webscope-mountain-tagged-set.pkl')
+    ids = [el['photo-id'] for el in result]
+    util.can(ids, 'webscope-mountain-tagged-ids.pkl')
+
+def fetch_mountain_ids(dat):
+    result = []
+    for el in dat:
+        tags = el['user-tags']
+        if (('mountain' in tags or
+             'mountains' in tags) and 
+            not el['video']):
+            result.append(el)
+    return result
