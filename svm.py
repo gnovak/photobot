7bbf207a8e251e705f1316196cfdc6e1d0dc8330
Greg Novak <greg.novak@gmail.com>
2014-09-11 17:41:07 -0700

    Make regularization return mean and err of CV

--- svm.py
+++ svm.py
@@ -1,10 +1,14 @@
 # Fit SVCs to the images
+import numpy as np
+
+import validation
 
 def regularization(vv,yy,models):
     result = []
+    means, errs = [], []
     for model in models:
-        values = loocv(vv,yy,model)
-        result.append(values.sum()/(1.0*len(values)))
-        #result.append(values.sum())
-    return result
+        mean, err = validation.kfoldcv(vv,yy,model, 5)
+        means.append(mean)
+        errs.append(err)
+    return np.array(means), np.array(errs)
 
